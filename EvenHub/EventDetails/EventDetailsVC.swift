488c3287107a3eaa0de10e14ab5db9f2403bf045
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-09-09 21:15:37 +0400

    setup scene delegate

---
+++ EvenHub/EventDetails/EventDetailsVC.swift
@@ -0,0 +1,21 @@
+//
+//  EventDetailsVC.swift
+//  EvenHub
+//
+//  Created by Ilnur on 09.09.2025.
+//
+
+import UIKit
+
+class EventDetailsVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+        
+    }
+    
+
+}
