9a8410971c6495ab7dfb51fc890155b30d7b612b
Ankor <ankor_socialki@mail.ru>
2025-09-10 17:30:11 +0300

    FEAT(ExploreVC): create ExploreCollectionViewCell

--- EvenHub/ExploreScreen/ExploreViewController.swift
+++ EvenHub/ExploreScreen/ExploreViewController.swift
@@ -11,6 +11,14 @@ class ExploreViewController: UIViewController {
     //MARK: - Properties
     
     //MARK: - UI Components
+    private lazy var exploreCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 237, height: 255)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        return collectionView
+    }()
     
     //MARK: - Lifecycle
     override func loadView() {
@@ -20,11 +28,14 @@ class ExploreViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupLayout()
     }
     //MARK: - Methods
     
     //MARK: - Setup UI
+    private func setupLayout() {
+        setupCollectionView()
+    }
     private func createBezier(on view: UIView, withColor color: UIColor) {
         let shapeLayer = CAShapeLayer()
         view.layer.addSublayer(shapeLayer)
@@ -35,5 +46,39 @@ class ExploreViewController: UIViewController {
                                 cornerRadii: CGSize(width: 33, height: 33))
         shapeLayer.path = path.cgPath
     }
+    private func setupCollectionView() {
+        view.addSubview(exploreCollectionView)
+        exploreCollectionView.delegate = self
+        exploreCollectionView.dataSource = self
+        exploreCollectionView.showsHorizontalScrollIndicator = false
+        exploreCollectionView.register(ExploreCollectionViewCell.self, forCellWithReuseIdentifier: ExploreCollectionViewCell.cellId)
+        exploreCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        exploreCollectionView.backgroundColor = .blue
+        
+        NSLayoutConstraint.activate([
+            exploreCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: view.frame.height * 0.3),
+            exploreCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            exploreCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            exploreCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.32)
+        ])
+        
+    }
 }
 
+//MARK: - ... Delegate and DataSource
+extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        7
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ExploreCollectionViewCell.cellId,
+            for: indexPath
+        ) as? ExploreCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.configure()
+        
+        return cell
+    }
+}
