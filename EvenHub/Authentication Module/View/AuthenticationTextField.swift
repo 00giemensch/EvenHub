9b88a47f92f37dba01ec4df35eb703e7306a5cfb
Zuev91 <zionix007@gmail.com>
2025-09-11 22:08:27 +0300

    setup AuthenticationTextField

--- EvenHub/Authentication Module/View/AuthenticationTextField.swift
+++ EvenHub/Authentication Module/View/AuthenticationTextField.swift
@@ -1,1 +1,22 @@
+import UIKit
 
+class AuthenticationTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private let image = UIImageView()
+    
+    private func setup() {
+        translatesAutoresizingMaskIntoConstraints = false
+        rightView = image
+        rightViewMode = .always
+        
+    }
+}
