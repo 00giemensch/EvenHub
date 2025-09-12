57631a2ae823a76e9f697479d3a66eee4f6b1cc6
Zuev91 <zionix007@gmail.com>
2025-09-12 03:13:27 +0300

    add appLabel

--- EvenHub/Authentication Module/Controllers/AuthenticationViewController.swift
+++ EvenHub/Authentication Module/Controllers/AuthenticationViewController.swift
@@ -22,14 +22,16 @@ class AuthenticationViewController: UIViewController {
     private let appLabel: UILabel = {
         let appLabel = UILabel()
         appLabel.translatesAutoresizingMaskIntoConstraints = false
-        appLabel.attributedText = Constants.Fonts.attributedString(for: Constants.appName, font: Constants.Fonts.bold, fontSize: 50, lineHeigh: 72)
+        appLabel.font = UIFont(name: Constants.Fonts.bold, size: 50)
+        appLabel.text = Constants.appName
+//        appLabel.attributedText = Constants.Fonts.attributedString(for: Constants.appName, font: Constants.Fonts.bold, fontSize: 50, lineHeigh: 72)
         return appLabel
     }()
     
     private let loginTextField: AuthenticationTextField = {
        let loginTextField = AuthenticationTextField()
         loginTextField.attributedPlaceholder = Constants.Fonts.attributedString(for: Constants.passwordPlaceholder, font: Constants.Fonts.book, fontSize: 14, lineHeigh: 23)
-//        loginTextField.updateImage(image: Constants.Icons.Authentication.mail!)
+        loginTextField.updateImage(image: Constants.Icons.Authentication.mail!)
         return loginTextField
     }()
     
@@ -39,13 +41,21 @@ class AuthenticationViewController: UIViewController {
     }
     
     func setupUI() {
-        view.backgroundColor = .systemGray
+        view.backgroundColor = Constants.Colors.PrimaryBlue.blue0
         view.addSubview(eventHubImage)
         NSLayoutConstraint.activate([
             eventHubImage.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -77),
-            eventHubImage.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 296),
+            eventHubImage.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -296),
             eventHubImage.widthAnchor.constraint(equalToConstant: 56),
             eventHubImage.heightAnchor.constraint(equalToConstant: 58)
+        ])
+        
+        view.addSubview(appLabel)
+        NSLayoutConstraint.activate([
+            appLabel.centerXAnchor.constraint(equalTo: eventHubImage.centerXAnchor),
+            appLabel.centerYAnchor.constraint(equalTo: eventHubImage.centerYAnchor, constant: 61),
+            appLabel.widthAnchor.constraint(equalToConstant: 150),
+            appLabel.heightAnchor.constraint(equalToConstant: 48)
         ])
     }
 }
