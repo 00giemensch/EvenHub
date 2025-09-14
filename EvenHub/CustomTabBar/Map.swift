96bbaab38eac5e0afffa20c6924bdab6d95d512a
Nikita Gritsunov <gritsunovnv@yandex.ru>
2025-09-14 23:24:50 +0300

    add CustomTabBar with tabbar_icons

---
+++ EvenHub/CustomTabBar/Map.swift
@@ -0,0 +1,17 @@
+//
+//  Events.swift
+//  EventsApp
+//
+//  Created by Никита Грицунов on 10.09.2025.
+//
+
+import UIKit
+
+class Map: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+}
