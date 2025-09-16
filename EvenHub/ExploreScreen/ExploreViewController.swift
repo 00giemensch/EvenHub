a21d52a37ea4cb5c4e5d40422225357d280eae22
Ankor <ankor_socialki@mail.ru>
2025-09-16 14:44:00 +0300

    FEAT(ExploreVC): Create location bar

--- EvenHub/ExploreScreen/ExploreViewController.swift
+++ EvenHub/ExploreScreen/ExploreViewController.swift
@@ -14,8 +14,15 @@ class ExploreViewController: UIViewController {
         cell.configure()
         return cell
     }
+    private let options = ["Option 1", "Option 2", "Option 3", "Option 4"]
+    private var isLocationListVisible = false
+    private var locationListHeightConstraint = NSLayoutConstraint()
+    private var tapOutsideGesture = UITapGestureRecognizer()
     
     //MARK: - UI Components
+    private let locationButton = UIButton()
+    private let locationLabel = UILabel()
+    private let locationList = UITableView()
     private let searchTextField = SearchTextField(scheme: .gray)
     private lazy var exploreCollectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
@@ -32,15 +39,43 @@ class ExploreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
-        
+        setupTapGesture()
     }
     //MARK: - Methods
+    private func setupTapGesture() {
+        tapOutsideGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapOutside))
+        tapOutsideGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapOutsideGesture)
+    }
+    
+    @objc private func changeLocationListVisible() {
+        isLocationListVisible.toggle()
+        if !isLocationListVisible {
+            locationList.isHidden = true
+            locationListHeightConstraint.constant = 0
+        } else {
+            UIView.animate(withDuration: 0.3) {
+                self.locationList.isHidden = false
+                let offset = self.options.count * 38 < 300  ? CGFloat(self.options.count * 38) : 300
+                self.locationListHeightConstraint.constant += offset
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    @objc private func handleTapOutside(_ gesture: UITapGestureRecognizer) {
+        let location = gesture.location(in: self.view)
+        guard isLocationListVisible,
+              !locationList.frame.contains(location),
+              !locationButton.frame.contains(location) else { return }
+        changeLocationListVisible()
+    }
     
     //MARK: - Setup UI
     private func setupLayout() {
         setupSearchTextField()
         setupCollectionView()
         setDataSource()
+        setupLocationBar()
     }
     private func createBezier(on view: UIView, withColor color: UIColor) {
         let shapeLayer = CAShapeLayer()
@@ -70,10 +105,76 @@ class ExploreViewController: UIViewController {
         snapshot.appendItems(Array(6...10), toSection: 2)
         dataSource.apply(snapshot)
     }
+    private func setupLocationBar() {
+        setupLocationButton()
+        setupLocationLabel()
+        setupLocationList()
+    }
+    private func setupLocationButton() {
+        view.addSubview(locationButton)
+        let titleText = NSMutableAttributedString()
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: Constants.Fonts.medium, size: 13) ?? .systemFont(ofSize: 13, weight: .light),
+            .foregroundColor: UIColor(resource: .blue10)
+        ]
+        titleText.append(NSAttributedString(string: "Current Location", attributes: attributes))
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(systemName: "arrowtriangle.down.fill")?.withTintColor(UIColor(resource: .blue0), renderingMode: .alwaysTemplate)
+        attachment.bounds = CGRect(x: 0, y: 0, width: 10, height: 5)
+        let imageString = NSAttributedString(attachment: attachment)
+        
+        titleText.append(imageString)
+        
+        
+        locationButton.setAttributedTitle(titleText, for: .normal)
+        locationButton.setTitleColor(.black, for: .normal)
+        locationButton.contentHorizontalAlignment = .left
+        locationButton.addTarget(self, action: #selector(changeLocationListVisible), for: .touchUpInside)
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalTo: searchTextField.leadingAnchor),
+            locationButton.widthAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    private func setupLocationLabel() {
+        view.addSubview(locationLabel)
+        locationLabel.text = "City"
+        locationLabel.font = UIFont(name: Constants.Fonts.medium, size: 13)
+        locationLabel.textColor = .white
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            locationLabel.topAnchor.constraint(equalTo: locationButton.bottomAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: locationButton.leadingAnchor)
+        ])
+    }
+    private func setupLocationList() {
+        view.addSubview(locationList)
+        locationList.delegate = self
+        locationList.dataSource = self
+        locationList.isHidden = true
+        locationList.separatorStyle = .none
+        locationList.layer.cornerRadius = 8
+        locationList.showsVerticalScrollIndicator = false
+        locationList.backgroundColor = Constants.Colors.Background.backgroundBlue
+        locationList.register(ExploreTableViewCell.self, forCellReuseIdentifier: ExploreTableViewCell.identifire)
+        locationList.translatesAutoresizingMaskIntoConstraints = false
+        locationListHeightConstraint = locationList.bottomAnchor.constraint(equalTo: locationButton.bottomAnchor)
+        locationListHeightConstraint.isActive = true
+        
+        NSLayoutConstraint.activate([
+            locationList.topAnchor.constraint(equalTo: locationButton.bottomAnchor),
+            locationList.leadingAnchor.constraint(equalTo: locationButton.leadingAnchor),
+            locationList.trailingAnchor.constraint(equalTo: searchTextField.trailingAnchor),
+            locationListHeightConstraint
+        ])
+    }
     private func setupSearchTextField() {
         view.addSubview(searchTextField)
         searchTextField.action = { [weak self] in
-        print("filter button tup")
+            print("filter button tup")
         }
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
         
@@ -81,8 +182,6 @@ class ExploreViewController: UIViewController {
             searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-//            searchTextField.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//            searchTextField.widthAnchor.constraint(equalToConstant: 100),
             searchTextField.heightAnchor.constraint(equalToConstant: 30)
         ])
     }
@@ -165,5 +264,24 @@ class ExploreViewController: UIViewController {
 
 //MARK: - CollectionView Delegate
 extension ExploreViewController: UICollectionViewDelegate {
-   
+    
+}
+
+//MARK: - TableView Delegate and DataSource
+extension ExploreViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ExploreTableViewCell.identifire, for: indexPath) as! ExploreTableViewCell
+        cell.configure(with: options[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("=\(options[indexPath.row])=")
+        locationLabel.text = options[indexPath.row]
+        isLocationListVisible = true
+        changeLocationListVisible()
+    }
 }
