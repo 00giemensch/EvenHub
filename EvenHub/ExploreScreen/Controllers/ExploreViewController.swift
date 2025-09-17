386fcf20014b1cb8e23d09109aa6acbe0bec8af4
Ankor <ankor_socialki@mail.ru>
2025-09-17 17:49:50 +0300

    FIX(ExploreVC): hide navigation bar

--- EvenHub/ExploreScreen/Controllers/ExploreViewController.swift
+++ EvenHub/ExploreScreen/Controllers/ExploreViewController.swift
@@ -59,6 +59,16 @@ class ExploreViewController: UIViewController {
             }
         }
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
     
     //MARK: - Methods
     private func setupTapGesture() {
