d73aa933f86b5fb195f2368f06bb5200c455af51
Ankor <ankor_socialki@mail.ru>
2025-09-22 04:41:31 +0300

    FEAT(ExploreVC): add coreData and Kingfisher

---
+++ EvenHub/SearchScreen/ViewModels/SearchViewModel.swift
@@ -0,0 +1,82 @@
+//
+//  SearchViewModel.swift
+//  EvenHub
+//
+//  Created by Andrei Kovryzhenko on 21.09.2025.
+//
+
+import UIKit
+
+final class SearchViewModel {
+    static let shared = SearchViewModel()
+    
+    //MARK: - Properties
+    private let key = "searchEvent"
+    var searchedEventsLoaded: (() -> Void)?
+    private lazy var dataManager = CoreDataManager.shared
+    private let apiService = EventAPIService()
+    private(set) var categories = [String]()
+    private(set) var searchedEvents = [FavoriteEvent]() {
+        didSet {
+            filtredEvents = searchedEvents
+        }
+    }
+    private(set)var filtredEvents = [FavoriteEvent]() {
+        didSet {
+            searchedEventsLoaded?()
+        }
+    }
+    
+    //MARK: - Lifecycle
+    private init(){}
+    
+    //MARK: - Private methods
+    private func fetchEventsById(searchResult: [SearchResultDTO]) async -> [EventDTO] {
+        var res = [EventDTO]()
+        self.categories = []
+        for searchEvent in searchResult {
+            do {
+                let event = try await apiService.getEventDetails(eventIDs: String(searchEvent.id), language: .en)
+                res += event
+                if let category = event.first?.categories {
+                    self.categories += category
+                }
+            } catch {
+                print("ExploreViewModel: \(#function)\nОшибка: \(error.localizedDescription)")
+            }
+        }
+        self.categories = Array(Set(categories))
+        return res
+    }
+    private func saveEventsToCoreData(_ events: [EventDTO]) {
+        dataManager.cacheEvents(events, cacheKey: key)
+        searchedEvents = dataManager.getCachedEvents(cacheKey: key)
+    }
+    //MARK: - Public methods
+    func searchEvent(with searchText: String) async {
+        do {
+            let eventsResponse = try await apiService.getSearchedEvents(with: searchText)
+            let events = await fetchEventsById(searchResult: eventsResponse.results)
+            await MainActor.run {
+                self.saveEventsToCoreData(events)
+            }
+        }
+        catch {
+            print("ExploreViewModel: \(#function)\nОшибка: \(error.localizedDescription)")
+        }
+    }
+    func checkStorage() {
+        dataManager.checkDatabaseStatus()
+    }
+    func clearSavedEvents() {
+        dataManager.clearCachedEvents(cacheKey: key)
+        searchedEvents.removeAll()
+    }
+    func filterByCategory(category: String) {
+        filtredEvents = searchedEvents.filter { $0.category == category }
+        searchedEvents.forEach {print($0.category)}
+    }
+    func removeFilterCategory() {
+        filtredEvents = searchedEvents
+    }
+}
